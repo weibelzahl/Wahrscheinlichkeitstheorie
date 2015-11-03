8020f877c9ba4acb1cd46b69006a504e73a9452f
weibelzahl <pfh>
2015-11-03 20:57:58 +0100

    Revert "Revert "swap table.txt with printing table""
    This reverts commit 9e7f2f5ee857479dc92f647f791eb4f99178121e.

--- Wahrscheinlichkeitsrechnung/table.R
+++ Wahrscheinlichkeitsrechnung/table.R
@@ -1,7 +1,3 @@
 # show cross table
-<<<<<<< HEAD
 print(table(studierende))
 #display_swirl_file("table.txt", "Wahrscheinlichkeitstheorie", "Wahrscheinlichkeitsrechnung")
-=======
-display_swirl_file("table.txt", "Wahrscheinlichkeitstheorie", "Wahrscheinlichkeitsrechnung")
->>>>>>> parent of 3dfd450... swap table.txt with printing table
