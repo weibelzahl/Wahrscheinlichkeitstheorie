9e7f2f5ee857479dc92f647f791eb4f99178121e
weibelzahl <pfh>
2015-11-03 20:53:37 +0100

    Revert "swap table.txt with printing table"
    This reverts commit 3dfd450f494c37094ae259446f0fee56df5a116e.
    
    Conflicts:
    	Wahrscheinlichkeitsrechnung/lesson.yaml
    	Wahrscheinlichkeitsrechnung/table.R

--- Wahrscheinlichkeitsrechnung/table.R
+++ Wahrscheinlichkeitsrechnung/table.R
@@ -1,3 +1,7 @@
 # show cross table
+<<<<<<< HEAD
 print(table(studierende))
 #display_swirl_file("table.txt", "Wahrscheinlichkeitstheorie", "Wahrscheinlichkeitsrechnung")
+=======
+display_swirl_file("table.txt", "Wahrscheinlichkeitstheorie", "Wahrscheinlichkeitsrechnung")
+>>>>>>> parent of 3dfd450... swap table.txt with printing table
