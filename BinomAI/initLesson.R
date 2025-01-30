93da7d9784f7127184217a15455508a5c0976df9
weibelzahl <stephan.weibelzahl@gmail.com>
2025-01-30 13:11:39 +0000

    first attempt do generate course with copilot

---
+++ BinomAI/initLesson.R
@@ -0,0 +1,8 @@
+# swirl-Motivations-Nachrichten zu unterdrücken
+options(swirl_is_fun = FALSE)
+options(swirl_language = "german")
+
+# Put initialization code in this file. The variables you create
+# here will show up in the user's workspace when he or she begins
+# the lesson.
+
